c723ea5d4709a65e73256e927ed5ce5ec8777697
shohei-kojima <26kojima@gmail.com>
2022-06-27 16:31:11 +0900

    20220627-163111

--- src/intersect.rs
+++ src/intersect.rs
@@ -341,7 +341,8 @@ pub fn intersect_a<'a, T1: Clone, T2>(a: &'a mut BedRecords<T1>,
         for r in &(a.map[chr]) {
             n = 0;
             for i in tree.find(r.start .. r.end) {
-                tmp_v.push(SimpleRange{ s: get_larger(&(r.start), &(i.interval().start)), e: get_smaller(&(r.end), &(i.interval().end))});
+                tmp_v.push(SimpleRange{ s: get_larger(&(r.start), &(i.interval().start)), 
+                                        e: get_smaller(&(r.end), &(i.interval().end))});
                 n += 1;
             }
             if n >= 1 {
